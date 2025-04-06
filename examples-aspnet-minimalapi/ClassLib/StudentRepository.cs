bfeefa410af47b34dca7aeeebe1fcc05944077dc
Xander Detruyer <xander.detruyer@student.howest.be>
2025-04-06 20:02:29 +0200

    should push

--- examples-aspnet-minimalapi/ClassLib/StudentRepository.cs
+++ examples-aspnet-minimalapi/ClassLib/StudentRepository.cs
@@ -18,7 +18,7 @@ namespace ClassLib
 
         public IEnumerable<Student> GetAllStudents()
         {
-            System.Console.WriteLine("GetAllStudents called");
+            System.Console.WriteLine("GetAllStudents called yayy!");
             return _students;
         }
 
